25b61ee4200cff081f9142c66876b0860bbb69fb
banidazin <banid@iocod.com>
2021-10-30 11:54:43 +0530

    show loading screen for 2 seconds

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, Text } from "react-native";
 
 export const Router = () => {
-    return (
+    const [isAppLoading, setAppLoading] = useState(true);
+
+    useEffect(() => {
+        const showAppLoadingScreen = async () => {
+            setTimeout(() => {
+                setAppLoading(false);
+            }, 2000);
+        };
+
+        showAppLoadingScreen();
+    }, []);
+
+    return (isAppLoading ? (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Router</Text>
+            <Text>Loading Screen</Text>
         </View>
-    );
+    ) : (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home Screen</Text>
+        </View>
+    ));
 };
