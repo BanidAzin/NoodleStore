fda7a1ec962a0be65e617205fcf1bc2843eda4ac
banidazin <banid@iocod.com>
2021-10-30 14:30:24 +0530

    add shimmering effect

---
+++ src/components/Shimmer.js
@@ -0,0 +1,45 @@
+import React, {useContext} from 'react';
+import {StyleSheet} from 'react-native';
+import ShimmerPlaceholder from 'react-native-shimmer-placeholder';
+import LinearGradient from 'react-native-linear-gradient';
+
+import {useDimensions} from '../helpers';
+
+export const ShimmerView = ({number = 4, shimmer, color = ['#121224', '#06060a', '#000000']}) => {
+  const {window} = useDimensions();
+
+  const {height, width} = window;
+
+  const mine = Array(number).fill(Math.random(0, 10));
+
+  return mine.map((key, index) => (
+    <ShimmerPlaceholder
+      key={index}
+      autoRun
+      LinearGradient={LinearGradient}
+      style={[
+        styles.shimmer,
+        {
+            minHeight: height * 0.3,
+            width: width * 0.95,
+            borderRadius: height * 0.02,
+        },
+        shimmer,
+      ]}
+      colorShimmer={color}
+    />
+  ))
+}
+
+const styles = StyleSheet.create({
+  shimmer: {
+    alignSelf: 'center',
+    margin: 20,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 1,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+})
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, {useEffect, useState} from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { Card } from "../components/Card";
+import { ShimmerView } from "../components/Shimmer";
 
 export const HomeScreen = () => {
-    return (
+    const [isLoading, setLoading] = useState(true);
+
+    return isLoading ? (
+        <ScrollView
+            scrollEnabled
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={styles.contentContainerStyle}>
+            <ShimmerView number={4}/>
+        </ScrollView>
+    ) : (
         <View style={styles.container}>
             <Card />
         </View>
@@ -13,5 +23,9 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    contentContainerStyle: {
+        flex: 1,
+        paddingVertical: '3%',
     },
 });
