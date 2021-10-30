b89b7256887c89e25098daaa600ba4099ece6ea4
banidazin <banid@iocod.com>
2021-10-30 14:00:44 +0530

    finish card design

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {View, Text, StyleSheet, ImageBackground} from "react-native";
+import Icon from "react-native-vector-icons/Feather";
 
 import { useDimensions } from "../helpers";
 
@@ -13,14 +14,6 @@ export const Card = props => {
             minHeight: height * 0.3,
             width: width * 0.95,
             borderRadius: height * 0.02,
-            alignSelf: 'center',
-            margin: 20,
-            backgroundColor: 'white',
-            shadowColor: 'black',
-            shadowOffset: {width: 0, height: 0},
-            shadowOpacity: 1,
-            shadowRadius: 8,
-            elevation: 8,
         }]}>
             <ImageBackground 
                 source={{uri: 'https://s3-ap-southeast-1.amazonaws.com/he-public-data/indexee3e8a8.jpeg'}}
@@ -31,35 +24,44 @@ export const Card = props => {
                     width: width * 0.95,
                 }}
             >
-                <View style={{position: 'absolute', top: 10, right: 15, backgroundColor: 'white', padding: 7, height: height * 0.04, width: width * 0.2, borderRadius: height * 0.05, alignItems: 'center', justifyContent: 'center'}}>
-                    <Text style={{fontSize: 18, fontWeight: '500', color: 'grey'}}>
-                        * 3.5
+                <View 
+                    style={[styles.ratingContainer, {
+                        height: height * 0.04,
+                        width: width * 0.2,
+                        borderRadius: height * 0.05,
+                    }]}
+                >
+                    <Icon name="star" size={height * 0.02}/>
+                    <Text style={[styles.text, {paddingLeft: 3}]}>
+                        3.5
                     </Text>
                 </View>
-                <View style={{position: 'absolute', bottom: 10, left: 10, backgroundColor: 'white', padding: 7, borderRadius: 5}}>
-                    <Text style={{fontSize: 18, fontWeight: '500', color: 'grey'}}>
+                <View style={styles.topTen}>
+                    <Text style={styles.text}>
                         2016 #1
                     </Text>
                 </View>
             </ImageBackground>
-            <View style={{marginVertical: 10, paddingHorizontal: 10}}>
-                <View style={{marginVertical: 2.5}}>
-                    <Text style={{fontSize: 20, fontWeight: '500'}}>
+
+            <View style={styles.detailsContainer}>
+                <View style={styles.detailsItem}>
+                    <Text style={styles.text}>
                         Oriental Style Instant Noodles Green Curry Flavour Jumbo Pack ("Pack")
                     </Text>
                 </View>
-                <View style={{marginVertical: 2.5}}>
-                    <Text style={{fontSize: 18, fontWeight: '500', color: 'grey'}}>
+                <View style={styles.detailsItem}>
+                    <Text style={styles.subText}>
                         Sapporo Ichiban
                     </Text>
                 </View>
-                <View style={{marginVertical: 2.5}}>
-                    <Text style={{fontSize: 18, fontWeight: '500', color: 'grey'}}>
+                <View style={[styles.detailsItem, {flexDirection: 'row'}]}>
+                    <Icon name="flag" size={height * 0.02} color="grey" />
+                    <Text style={styles.subText}>
                         Singapore
                     </Text>
                 </View>
-                <View style={{marginVertical: 2.5}}>
-                    <Text style={{fontSize: 18, fontWeight: '500', color: 'grey'}}>
+                <View style={styles.detailsItem}>
+                    <Text style={styles.subText}>
                         2016 #1
                     </Text>
                 </View>
@@ -70,5 +72,49 @@ export const Card = props => {
 };
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        alignSelf: 'center',
+        margin: 20,
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 1,
+        shadowRadius: 8,
+        elevation: 8,
+    },
+    ratingContainer: {
+        position: 'absolute',
+        top: 10,
+        right: 15,
+        backgroundColor: 'white',
+        padding: 7,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: '500',
+    },
+    topTen: {
+        position: 'absolute',
+        bottom: 10,
+        left: 10,
+        backgroundColor: 'white',
+        padding: 7,
+        borderRadius: 5,
+    },
+    detailsContainer: {
+        marginVertical: 10,
+        paddingHorizontal: 10,
+    },
+    detailsItem: {
+        marginVertical: 2.5,
+    },
+    subText: {
+        fontSize: 18,
+        fontWeight: '500',
+        color: 'grey',
+        paddingLeft: 3,
+    },
 });
